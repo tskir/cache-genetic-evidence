01843dcf32320b4804f8a2ab17c128f04f373aac
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2021-05-11 13:37:09 +0100

    Load upstream targets and UniProt to Ensembl mappings

--- README.md
+++ README.md
@@ -8,24 +8,38 @@ python3 -m pip -q install -r requirements.txt
 
 ## Download the data
 ```bash
+# Open Targets Platform release.
 export RELEASE=21.04
 export DATASETS="targets diseases associationByDatatypeDirect"
 for DATASET in ${DATASETS}; do
   wget --recursive --no-parent --no-host-directories --cut-dirs 8 \
     ftp://ftp.ebi.ac.uk/pub/databases/opentargets/platform/${RELEASE}/output/etl/parquet/${DATASET}
 done
+
+# UniProt to Ensembl mapping.
+echo -e 'UniProt\tEnsembl' > uniprot_ensembl_mapping.tsv
+wget -qO- https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/idmapping/by_organism/HUMAN_9606_idmapping_selected.tab.gz \
+  | gzip -cd | cut -d$'\t' -f1,19 >> uniprot_ensembl_mapping.tsv
 ```
+
+The input list of targets is generated upstream and shared over email.
 
 ## Run
 ```bash
 source env/bin/activate
-python3 cache-genetic-evidence.py
+python3 cache-genetic-evidence.py \
+  --upstream-targets CACHE_List_SAR_GWAS_DiseaseV2.csv \
+  --uniprot-ensembl-mapping uniprot_ensembl_mapping.tsv
 ```
 
 ## Current results
 ```
-Total targets: 60608
-  Of them, protein coding: 19933
-Total diseases: 18497
-  Of them, genetic diseases: 8147
+Total UniProt AC → Ensembl gene ID mappings: 81,097
+
+Total OT targets: 60,608
+  Of them, protein coding: 19,933
+Total OT diseases: 18,497
+  Of them, genetic diseases: 8,147
+
+Total upstream targets: 6,742
 ```
--- cache-genetic-evidence.py
+++ cache-genetic-evidence.py
@@ -1,22 +1,43 @@
 #!/usr/bin/env python3
 
+import argparse
+
 import pyspark
 import pyspark.sql.functions as pf
 
-spark = pyspark.sql.SparkSession.builder.appName('cache_genetic_evidence').getOrCreate()
-datasets = {d: spark.read.parquet(d) for d in ('targets', 'diseases', 'associationByDatatypeDirect')}
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--upstream-targets', required=True,
+    help='A CSV list of targets, generated upstream.'
+)
+parser.add_argument(
+    '--uniprot-ensembl-mapping', required=True,
+    help='A TSV mapping from UniProt AC to Ensembl gene ID.'
+)
+args = parser.parse_args()
 
-# Filter only protein coding targets.
+# Initialise the Spark session and ingest the data.
+spark = pyspark.sql.SparkSession.builder.appName('cache_genetic_evidence').getOrCreate()
+ot_datasets = {d: spark.read.parquet(d) for d in ('targets', 'diseases', 'associationByDatatypeDirect')}
+upstream_targets = spark.read.csv(args.upstream_targets, header=True)
+uniprot_ensembl_mapping = (
+    spark.read.csv(args.uniprot_ensembl_mapping, header=True, sep='\t')
+    .withColumn('Ensembl', pf.split(pf.col('Ensembl'), '; '))
+    .withColumn('Ensembl', pf.explode('Ensembl'))
+    .filter(pf.col('Ensembl').isNotNull())
+)
+
+# OT platform: Filter only protein coding targets.
 coding_targets = (
-    datasets['targets']
+    ot_datasets['targets']
     .filter(pf.col('bioType') == 'protein_coding')
     .select('id')
     .distinct()
 )
 
-# Filter only genetic, familial or congenital diseases.
+# OT platform: Filter only genetic, familial or congenital diseases.
 genetic_diseases = (
-    datasets['diseases']
+    ot_datasets['diseases']
     .withColumn('therapeuticArea', pf.explode('therapeuticAreas'))
     .filter(pf.col('therapeuticArea') == 'OTAR_0000018')
     .select('id')
@@ -24,8 +45,12 @@ genetic_diseases = (
 )
 
 print(f"""
-Total targets: {datasets['targets'].count()}
-  Of them, protein coding: {coding_targets.count()}
-Total diseases: {datasets['diseases'].count()}
-  Of them, genetic diseases: {genetic_diseases.count()}
+Total UniProt AC → Ensembl gene ID mappings: {uniprot_ensembl_mapping.count():,}
+
+Total OT targets: {ot_datasets['targets'].count():,}
+  Of them, protein coding: {coding_targets.count():,}
+Total OT diseases: {ot_datasets['diseases'].count():,}
+  Of them, genetic diseases: {genetic_diseases.count():,}
+
+Total upstream targets: {upstream_targets.count():,}
 """)
